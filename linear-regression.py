39e39a446db01bc4c6c82560ffce6dc8e33789d4
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-12 22:24:18 +0530

    plot scatter plot

--- linear-regression.py
+++ linear-regression.py
@@ -1,5 +1,9 @@
 import pandas  as pd 
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 insurance_data = pd.read_csv("insurance.csv")
-print(insurance_data)
+# print(insurance_data)
+
+sns.scatterplot(x = insurance_data["bmi"],y = insurance_data["charges"],hue = insurance_data["smoker"])
+plt.show()
