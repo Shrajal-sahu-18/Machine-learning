570711e0f861481ccec66ee87f70cb24174672d4
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-12 22:59:37 +0530

    split dataset into training and testing sets

--- linear-regression.py
+++ linear-regression.py
@@ -18,3 +18,7 @@ X["smoker"] = X["smoker"].map({"yes":1,"no":0})
 
 #Train test split
 from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
+
