e1fb340fcd320424146c551601fecf3f91491afc
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-12 22:34:23 +0530

    split dataset into X and y

--- linear-regression.py
+++ linear-regression.py
@@ -6,4 +6,9 @@ insurance_data = pd.read_csv("insurance.csv")
 # print(insurance_data)
 
 sns.scatterplot(x = insurance_data["bmi"],y = insurance_data["charges"],hue = insurance_data["smoker"])
-plt.show()
+# plt.show()
+
+X = insurance_data.drop(columns = ["charges","region"])
+y = insurance_data["charges"]
+
+
