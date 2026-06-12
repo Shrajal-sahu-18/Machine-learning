12e2b7ebd6974bba1560b9237efd2220e86be66e
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-12 23:01:53 +0530

    import LinearRegression from sklearn

--- linear-regression.py
+++ linear-regression.py
@@ -22,3 +22,5 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42)
 
+from sklearn.linear_model import LinearRegression
+
