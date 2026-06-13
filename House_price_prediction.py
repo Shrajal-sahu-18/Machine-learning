5ca30bc52706559cbd760463f673ca1c3133fab8
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-13 11:43:29 +0530

    import library

---
+++ House_price_prediction.py
@@ -0,0 +1,9 @@
+# import library
+import pandas as pd
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from sklearn.metrics import mean_absolute_percentage_error
+import numpy as np
+from sklearn.preprocessing import StandardScaler
