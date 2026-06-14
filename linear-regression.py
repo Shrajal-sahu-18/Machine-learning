95ed48c82966d55a88645c9efc22472c2816f7b0
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-14 23:00:01 +0530

    train linearregression model

--- linear-regression.py
+++ linear-regression.py
@@ -55,3 +55,7 @@ X["smoker"] = X["smoker"].map({"yes":1,"no":0})
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42)
+
+
+model = LinearRegression()
+model.fit(X_train,y_train)
