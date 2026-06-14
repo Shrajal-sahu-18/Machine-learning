dd1f8253a4fda07d857cf2acb47998055865b9ea
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-14 22:58:51 +0530

    Train test split

--- linear-regression.py
+++ linear-regression.py
@@ -52,3 +52,6 @@ y = insurance_data["charges"]
 X = pd.get_dummies(X,columns = ["region"],drop_first = True,dtype = int)
 X["sex"] = X["sex"].map({"female":1,"male":0})  # convert female,male,1,0
 X["smoker"] = X["smoker"].map({"yes":1,"no":0})
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
