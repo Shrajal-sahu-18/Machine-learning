a1c692088c2290bc4b9d85c7d04a257ab1b170a9
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-15 14:08:59 +0530

    import library

---
+++ Supervised_learning/Lasso_regression.py
@@ -0,0 +1,4 @@
+from sklearn.linear_model import Lasso #$linear_model
+from sklearn.metrics import mean_squared_error,r2_score
+import pandas as pd
+from sklearn.model_selection import train_test_split
