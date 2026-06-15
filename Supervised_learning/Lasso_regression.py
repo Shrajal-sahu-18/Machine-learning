db90ef934506d21333a74c58788cf9c3240fc1eb
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-15 14:09:57 +0530

    Load data

--- Supervised_learning/Lasso_regression.py
+++ Supervised_learning/Lasso_regression.py
@@ -2,3 +2,6 @@ from sklearn.linear_model import Lasso #$linear_model
 from sklearn.metrics import mean_squared_error,r2_score
 import pandas as pd
 from sklearn.model_selection import train_test_split
+
+#load data
+insurance_data = pd.read_csv("insurance.csv")
