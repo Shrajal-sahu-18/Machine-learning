8a18b5889d6bdcfb6bf003ac2d7780e57a19a282
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-16 22:49:37 +0530

    Load Dataset

--- Supervised_learning/Logistic_regression.py
+++ Supervised_learning/Logistic_regression.py
@@ -3,3 +3,5 @@ import seaborn as sns
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score , precision_score
+
+heart_df = pd.read_csv("heart.csv")
