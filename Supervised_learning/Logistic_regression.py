8a1a01005484b7fae19dffcb73bb940d275c350e
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-16 22:52:38 +0530

    train test split

--- Supervised_learning/Logistic_regression.py
+++ Supervised_learning/Logistic_regression.py
@@ -16,3 +16,8 @@ X = heart_df.drop("target",axis = 1)
 y = heart_df["target"]
 X.head()
 y.head()
+
+#tain test split
+X_train, X_test,y_train,y_test = train_test_split(
+    X,y ,test_size = 0.2 , random_state = 42
+)
