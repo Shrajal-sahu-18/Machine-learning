6a208f0b99e90b1181835027dd15b8f977b8e24a
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-17 12:58:43 +0530

    import standard scaler from sklearn

--- Supervised_learning/House_price_prediction.py
+++ Supervised_learning/House_price_prediction.py
@@ -60,3 +60,6 @@ print("r2_score:",r2_score(y_test,y_pred))
 print("mae:",mean_absolute_error(y_test,y_pred))
 print(" root mean sqared error:",np.sqrt(mean_squared_error(y_test,y_pred)))
 print(mean_absolute_percentage_error(y_test, y_pred))
+
+# Feature Scaling - to try & improve baseline performance
+from sklearn.preprocessing import StandardScaler
