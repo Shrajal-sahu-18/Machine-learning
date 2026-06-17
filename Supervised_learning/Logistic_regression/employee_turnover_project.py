5dc83e02dcc8518f6e765ab257ac1765a9e2550a
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-17 16:13:20 +0530

    Split Dataset Into Two parts

--- Supervised_learning/Logistic_regression/employee_turnover_project.py
+++ Supervised_learning/Logistic_regression/employee_turnover_project.py
@@ -5,3 +5,7 @@ from sklearn.metrics import accuracy_score,classification_report
 
 #Load Dataset
 df = pd.read_csv("employee_turnover.csv")
+
+#Split Dataset into two parts
+X = df.drop(["Employee_Turnover"],axis = 1)
+y = df["Employee_Turnover"]
