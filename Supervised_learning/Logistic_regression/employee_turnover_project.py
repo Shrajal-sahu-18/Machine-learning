4ea042688be3f9c3859972cfbc63dd34ad2201d6
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-17 16:12:22 +0530

    load Dataset

--- Supervised_learning/Logistic_regression/employee_turnover_project.py
+++ Supervised_learning/Logistic_regression/employee_turnover_project.py
@@ -2,3 +2,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score,classification_report
+
+#Load Dataset
+df = pd.read_csv("employee_turnover.csv")
