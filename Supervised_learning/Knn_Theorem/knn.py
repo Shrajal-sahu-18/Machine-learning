a2f7a5b381892afcf3323408e78e8b5f077fc912
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-18 19:20:01 +0530

    Train test split

--- Supervised_learning/Knn_Theorem/knn.py
+++ Supervised_learning/Knn_Theorem/knn.py
@@ -11,3 +11,8 @@ heart_df.head()
 #split dataset into parts
 X = heart_df.drop(["target"],axis = 1)
 y = heart_df["target"]
+
+#Train Test Split
+X_train,X_test,y_train,y_test = train_test_split(
+    X,y ,test_size = 0.2,random_state = 42
+)
