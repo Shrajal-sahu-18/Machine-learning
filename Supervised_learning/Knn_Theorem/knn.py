813d7948e4c4067ea0c8828198d49279091f2971
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-18 19:21:16 +0530

    scaled train data

--- Supervised_learning/Knn_Theorem/knn.py
+++ Supervised_learning/Knn_Theorem/knn.py
@@ -16,3 +16,7 @@ y = heart_df["target"]
 X_train,X_test,y_train,y_test = train_test_split(
     X,y ,test_size = 0.2,random_state = 42
 )
+
+#Scaled data
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
