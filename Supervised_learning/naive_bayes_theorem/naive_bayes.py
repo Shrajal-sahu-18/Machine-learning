361a7b24edf083836927e8fe662165f1519d897d
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-18 16:27:53 +0530

    Train test split

--- Supervised_learning/naive_bayes_theorem/naive_bayes.py
+++ Supervised_learning/naive_bayes_theorem/naive_bayes.py
@@ -11,3 +11,7 @@ heart_df.head()
 #Split dataset into two parts
 X = heart_df.drop(["target"] , axis = 1)
 y = heart_df["target"]
+
+X_train,X_test,y_train,y_test = train_test_split(
+    X,y , test_size = 0.2,random_state = 42
+)
