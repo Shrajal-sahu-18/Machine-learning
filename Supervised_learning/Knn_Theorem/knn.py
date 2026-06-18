f5db975d3dcc7f88444ed58b965e4af6628123f4
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-18 19:24:17 +0530

    precision_score,accuracy_score,recall_score

--- Supervised_learning/Knn_Theorem/knn.py
+++ Supervised_learning/Knn_Theorem/knn.py
@@ -27,3 +27,8 @@ knn_classifier = KNeighborsClassifier(n_neighbors=3)
 knn_classifier.fit(X_train_scaled,y_train)
 
 y_pred = knn_classifier.predict(X_test_scaled)
+
+#Evaluation
+print("precision_score:",precision_score(y_test,y_pred))
+print("accuracy_score:",accuracy_score(y_test,y_pred))
+print("recall_score:",recall_score(y_test,y_pred))
