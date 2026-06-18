7213c82fd1c2bbb00c8cabc75ea9f74351acdb33
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-18 16:26:20 +0530

    Load dataset

--- Supervised_learning/naive_bayes_theorem/naive_bayes.py
+++ Supervised_learning/naive_bayes_theorem/naive_bayes.py
@@ -2,3 +2,8 @@ import pandas as pd
 from  sklearn.metrics import precision_score,accuracy_score,f1_score,recall_score
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
+
+#Load datset
+heart_df = pd.read_csv("heart.csv")
+
+heart_df.head()
