e193630cf7c6dbf2075c462f5dd7f64129dc3610
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-18 16:21:13 +0530

    import module

---
+++ Supervised_learning/naive_bayes_theorem/naive_bayes.py
@@ -0,0 +1,4 @@
+import pandas as pd
+from  sklearn.metrics import precision_score,accuracy_score,f1_score,recall_score
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
