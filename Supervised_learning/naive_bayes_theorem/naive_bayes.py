7b6474ceaf55a612a9faf2832d6ca436ae8f3e82
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-18 16:28:35 +0530

    Train Guassian naive bayes model

--- Supervised_learning/naive_bayes_theorem/naive_bayes.py
+++ Supervised_learning/naive_bayes_theorem/naive_bayes.py
@@ -15,3 +15,7 @@ y = heart_df["target"]
 X_train,X_test,y_train,y_test = train_test_split(
     X,y , test_size = 0.2,random_state = 42
 )
+
+#naive bayes
+gnb_model = GaussianNB()
+gnb_model.fit(X_train,y_train)
