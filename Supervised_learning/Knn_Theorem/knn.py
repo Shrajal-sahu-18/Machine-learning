5394f06c4f2987ffbb48b3e9918d32614f9a4899
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-18 19:16:21 +0530

    Load dataset

--- Supervised_learning/Knn_Theorem/knn.py
+++ Supervised_learning/Knn_Theorem/knn.py
@@ -3,3 +3,7 @@ from sklearn.metrics import precision_score,accuracy_score,recall_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
+
+#Load data
+heart_df = pd.read_csv("heart.csv")
+heart_df.head()
