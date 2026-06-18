57ac330db869a86619bb9b5b43b2cd7438c02006
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-18 19:21:51 +0530

    scaled test data

--- Supervised_learning/Knn_Theorem/knn.py
+++ Supervised_learning/Knn_Theorem/knn.py
@@ -20,3 +20,4 @@ X_train,X_test,y_train,y_test = train_test_split(
 #Scaled data
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled =  scaler.transform(X_test)
