cf5babe9b3af4df9f21e78838debee0785ef7801
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-19 16:50:29 +0530

    train test split

--- Supervised_learning/Knn_Theorem/knn.py
+++ Supervised_learning/Knn_Theorem/knn.py
@@ -85,3 +85,6 @@ print(classifierCV.best_params_)
 
 #Pipeline
 from sklearn.pipeline import Pipeline
+X_train,X_test,y_train,y_test = train_test_split(
+    X,y ,test_size = 0.2,random_state = 42
+)
