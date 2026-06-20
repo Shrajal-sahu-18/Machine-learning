1499f0ba5474810712c8f0193f3a9c7887b9ea7b
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-20 23:34:34 +0530

    train test split

--- Supervised_learning/Minor_project/iris_project.py
+++ Supervised_learning/Minor_project/iris_project.py
@@ -13,3 +13,7 @@ from sklearn.model_selection import train_test_split
 
 X = df.drop(["Species"],axis = 1)
 y = df["Species"]
+
+X_train,X_test,y_tarin,y_test = train_test_split(
+    X,y , test_size = 0.2 , random_state = 42
+)
