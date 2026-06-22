6bff3aead3bd0095a36ae34dc27c5bf3db73e060
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-22 22:57:13 +0530

    Load dataset

--- Supervised_learning/Minor_project/credit_wise_loan_system.py
+++ Supervised_learning/Minor_project/credit_wise_loan_system.py
@@ -3,3 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
+
+
+df = pd.read_csv("loan_approval_data.csv")
+print(df.head())
