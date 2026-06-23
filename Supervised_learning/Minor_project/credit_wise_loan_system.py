e556861026497c73e285dbd36b586c295f491fc3
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-23 23:12:58 +0530

    import onehotencode ,labelencoder

--- Supervised_learning/Minor_project/credit_wise_loan_system.py
+++ Supervised_learning/Minor_project/credit_wise_loan_system.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
-
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
 
 df = pd.read_csv("loan_approval_data.csv")
 # print(df.head())
