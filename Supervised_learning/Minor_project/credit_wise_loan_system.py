f7322428924c3cea7a100f261a211626d2d72ef0
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-24 23:11:13 +0530

    Train linearregression model

--- Supervised_learning/Minor_project/credit_wise_loan_system.py
+++ Supervised_learning/Minor_project/credit_wise_loan_system.py
@@ -173,3 +173,7 @@ X_train,X_test,y_train,y_test = train_test_split(
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
+
+#Train linearregression model
+logs_model = LogisticRegression()
+logs_model.fit(X_train_scaled,y_train)
