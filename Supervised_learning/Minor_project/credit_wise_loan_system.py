4bfa90caa50f855d9c580ed1be45e2f044320bdb
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-25 23:29:03 +0530

    train knnclassifier

--- Supervised_learning/Minor_project/credit_wise_loan_system.py
+++ Supervised_learning/Minor_project/credit_wise_loan_system.py
@@ -250,3 +250,10 @@ print("recall score:",recall_score(y_test,y_pred))
 print("f1 score:",f1_score(y_test,y_pred))
 print("confusion matrix:",confusion_matrix(y_test,y_pred))
 print("Accuracy score:",accuracy_score(y_test,y_pred))
+
+
+
+# knn classifier
+from sklearn.neighbors import KNeighborsClassifier
+knn_model = KNeighborsClassifier(n_neighbors=5)
+knn_model.fit(X_train_scaled,y_train)
