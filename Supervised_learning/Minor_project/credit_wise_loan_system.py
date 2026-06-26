3f11fd8b44771c940b113d2df1f219d03fa58c24
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-26 23:00:36 +0530

    Train Model

--- Supervised_learning/Minor_project/credit_wise_loan_system.py
+++ Supervised_learning/Minor_project/credit_wise_loan_system.py
@@ -312,3 +312,7 @@ X_train,X_test,y_train,y_test = train_test_split(
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
+
+naive_model = GaussianNB()
+naive_model.fit(X_train_scaled,y_train)
+naive_model.predict(X_test_scaled)
