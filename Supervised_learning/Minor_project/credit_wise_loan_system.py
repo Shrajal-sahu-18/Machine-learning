3b551a803e6575224ffe549d5acb306a173cbd06
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-06-26 22:55:11 +0530

    read csv file

--- Supervised_learning/Minor_project/credit_wise_loan_system.py
+++ Supervised_learning/Minor_project/credit_wise_loan_system.py
@@ -10,293 +10,294 @@ from sklearn.metrics import precision_score,confusion_matrix,recall_score,f1_sco
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 
-df = pd.read_csv("loan_approval_data.csv")
-# print(df.head())
+# df = pd.read_csv("loan_approval_data.csv")
+# # print(df.head())
 
-df.head()
-df.info()
-df.isnull().sum()
-df.describe()
+# df.head()
+# df.info()
+# df.isnull().sum()
+# df.describe()
 
-# Handle missing value
-categorical_cols = df.select_dtypes(include = "object").columns
-numerical_cols = df.select_dtypes(include = "float64").columns 
+# # Handle missing value
+# categorical_cols = df.select_dtypes(include = "object").columns
+# numerical_cols = df.select_dtypes(include = "float64").columns 
 
-categorical_cols.size + numerical_cols.size 
+# categorical_cols.size + numerical_cols.size 
 
-from sklearn.impute import SimpleImputer
+# from sklearn.impute import SimpleImputer
 
-num_imp = SimpleImputer(strategy = "mean")
-df[numerical_cols] = num_imp.fit_transform(df[numerical_cols])
+# num_imp = SimpleImputer(strategy = "mean")
+# df[numerical_cols] = num_imp.fit_transform(df[numerical_cols])
 
-categorical_imp = SimpleImputer(strategy = "most_frequent")
-df[categorical_cols] = categorical_imp.fit_transform(df[categorical_cols])
+# categorical_imp = SimpleImputer(strategy = "most_frequent")
+# df[categorical_cols] = categorical_imp.fit_transform(df[categorical_cols])
 
-#EDA(Exploratory Data Analysis)
+# #EDA(Exploratory Data Analysis)
 
-# how balanced our classes are
-classes_count = df["Loan_Approved"].value_counts()
-plt.pie(classes_count , labels = ["No","Yes"], autopct = "%1.1f%%")
-plt.title("Is loan approved or not")
+# # how balanced our classes are
+# classes_count = df["Loan_Approved"].value_counts()
+# plt.pie(classes_count , labels = ["No","Yes"], autopct = "%1.1f%%")
+# plt.title("Is loan approved or not")
 
-gender_cnt = df["Gender"].value_counts()
-ax =sns.barplot(gender_cnt)
-ax.bar_label(ax.containers[0])
-
-
-edu_cnt = df["Education_Level"].value_counts()
-ax = sns.barplot(edu_cnt)
-ax.bar_label(ax.containers[0])
-
-emp_cunt = df["Employment_Status"].value_counts()
-ax = sns.barplot(emp_cunt)
-ax.bar_label(ax.containers[0])
+# gender_cnt = df["Gender"].value_counts()
+# ax =sns.barplot(gender_cnt)
+# ax.bar_label(ax.containers[0])
 
 
+# edu_cnt = df["Education_Level"].value_counts()
+# ax = sns.barplot(edu_cnt)
+# ax.bar_label(ax.containers[0])
 
-martial_cnt = df["Marital_Status"].value_counts()
-ax = sns.barplot(martial_cnt)
-ax.bar_label(ax.containers[0])
-
-purpose_cnt = df["Loan_Purpose"].value_counts()
-ax = sns.barplot(purpose_cnt)
-ax.bar_label(ax.containers[0])
-
-
-property_area_cnt =df["Property_Area"].value_counts()
-ax = sns.barplot(property_area_cnt)
-ax.bar_label(ax.containers[0])
-
-
-empl_cate_cnt = df["Employer_Category"].value_counts()
-ax = sns.barplot(empl_cate_cnt)
-ax.bar_label(ax.containers[0])
+# emp_cunt = df["Employment_Status"].value_counts()
+# ax = sns.barplot(emp_cunt)
+# ax.bar_label(ax.containers[0])
 
 
 
-#Analyze Income
-sns.histplot(
-    data = df,
-    x = "Applicant_Income",
-    bins = 20
+# martial_cnt = df["Marital_Status"].value_counts()
+# ax = sns.barplot(martial_cnt)
+# ax.bar_label(ax.containers[0])
+
+# purpose_cnt = df["Loan_Purpose"].value_counts()
+# ax = sns.barplot(purpose_cnt)
+# ax.bar_label(ax.containers[0])
+
+
+# property_area_cnt =df["Property_Area"].value_counts()
+# ax = sns.barplot(property_area_cnt)
+# ax.bar_label(ax.containers[0])
+
+
+# empl_cate_cnt = df["Employer_Category"].value_counts()
+# ax = sns.barplot(empl_cate_cnt)
+# ax.bar_label(ax.containers[0])
+
+
+
+# #Analyze Income
+# sns.histplot(
+#     data = df,
+#     x = "Applicant_Income",
+#     bins = 20
     
-)
+# )
 
 
-sns.histplot(
-    data = df,
-    x = "Coapplicant_Income",
-    bins = 20
-)
+# sns.histplot(
+#     data = df,
+#     x = "Coapplicant_Income",
+#     bins = 20
+# )
 
 
-#Outliers - box_plot
-sns.boxplot(
-    data = df,
-    x = "Loan_Approved",
-    y = "Applicant_Income"
-)
+# #Outliers - box_plot
+# sns.boxplot(
+#     data = df,
+#     x = "Loan_Approved",
+#     y = "Applicant_Income"
+# )
 
-fig,axes = plt.subplots(2,3)
+# fig,axes = plt.subplots(2,3)
 
-sns.boxplot(ax = axes[0,0], data = df,x = "Loan_Approved",y = "Applicant_Income")
-sns.boxplot(ax = axes[0,1], data = df,x = "Loan_Approved", y = "Credit_Score")
-sns.boxplot(ax = axes[0,2],data = df,x = "Loan_Approved",y = "Age")
-sns.boxplot(ax = axes[1,0], data = df, x = "Loan_Approved", y = "DTI_Ratio")
-sns.boxplot(ax = axes[1,1], data = df , x = "Loan_Approved", y = "Savings")
-sns.boxplot(ax = axes[1,2],data = df,x = "Loan_Approved",y = "Loan_Amount")
+# sns.boxplot(ax = axes[0,0], data = df,x = "Loan_Approved",y = "Applicant_Income")
+# sns.boxplot(ax = axes[0,1], data = df,x = "Loan_Approved", y = "Credit_Score")
+# sns.boxplot(ax = axes[0,2],data = df,x = "Loan_Approved",y = "Age")
+# sns.boxplot(ax = axes[1,0], data = df, x = "Loan_Approved", y = "DTI_Ratio")
+# sns.boxplot(ax = axes[1,1], data = df , x = "Loan_Approved", y = "Savings")
+# sns.boxplot(ax = axes[1,2],data = df,x = "Loan_Approved",y = "Loan_Amount")
 
-plt.tight_layout()
+# plt.tight_layout()
 
-#credit score with loan approved
-sns.histplot(
-    data = df,
-    x = "Credit_Score",
-    hue = "Loan_Approved",
-    bins = 20,
-    multiple = "dodge" 
-    # side by side dodge ka mtlb alag alag category ke bars ko alag alag dikhana
-)
-
-
-sns.histplot(
-    data = df,
-    x = "Applicant_Income",
-    hue = "Loan_Approved",
-    multiple = "dodge",
-    bins = 20
-)
-
-#Remove_applicant_id
-df = df.drop("Applicant_ID",axis = 1)
-
-#Encoding 
-df.head()
-df.columns
-df.info()
+# #credit score with loan approved
+# sns.histplot(
+#     data = df,
+#     x = "Credit_Score",
+#     hue = "Loan_Approved",
+#     bins = 20,
+#     multiple = "dodge" 
+#     # side by side dodge ka mtlb alag alag category ke bars ko alag alag dikhana
+# )
 
 
-#labelEncoder
-le = LabelEncoder()
-df["Education_Level"] = le.fit_transform(df["Education_Level"])
-df["Loan_Approved"] = le.fit_transform(df["Loan_Approved"])
+# sns.histplot(
+#     data = df,
+#     x = "Applicant_Income",
+#     hue = "Loan_Approved",
+#     multiple = "dodge",
+#     bins = 20
+# )
 
-#oNEhOTeNCODING
-cols = ["Employment_Status","Marital_Status","Loan_Purpose","Property_Area","Gender","Employer_Category"]
-ohe = OneHotEncoder(drop = "first",sparse_output = False,handle_unknown = "ignore")
-encoded = ohe.fit_transform(df[cols])
-encoded_df = pd.DataFrame(encoded,columns = ohe.get_feature_names_out(cols),index = df.index )
-df = pd.concat([df.drop(columns = cols),encoded_df],axis = 1)
+# #Remove_applicant_id
+# df = df.drop("Applicant_ID",axis = 1)
 
-
-#correlation heat map
-num_cols = df.select_dtypes(include = "number")
-corr_matrix = num_cols.corr()
-plt.figure(figsize = (15,8))
-sns.heatmap(
-    corr_matrix,
-    annot = True,
-    fmt = ".2f",
-    cmap = "coolwarm"
-)
-num_cols.corr()["Loan_Approved"].sort_values(ascending = False)
-# corr_matrix
-
-#split dataset 
-X = df.drop(["Loan_Approved"],axis = 1)
-y = df["Loan_Approved"]
-
-#train test split
-X_train,X_test,y_train,y_test = train_test_split(
-    X,y, test_size = 0.2 , random_state  =42
-)
-
-#Standard Scale
-scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
-
-#Train linearregression model
-logs_model = LogisticRegression()
-logs_model.fit(X_train_scaled,y_train)
-y_pred = logs_model.predict(X_test_scaled)
-
-# Evaluation
-print("precision score:",precision_score(y_test,y_pred))
-print("recall score:",recall_score(y_test,y_pred))
-print("f1 score:",f1_score(y_test,y_pred))
-print("confusion matrix:",confusion_matrix(y_test,y_pred))
-print("Accuracy score:",accuracy_score(y_test,y_pred))
+# #Encoding 
+# df.head()
+# df.columns
+# df.info()
 
 
-#knn model
-knn_model = KNeighborsClassifier(n_neighbors=5)
-knn_model.fit(X_train_scaled,y_train)
-y_pred = knn_model.predict(X_test_scaled)
+# #labelEncoder
+# le = LabelEncoder()
+# df["Education_Level"] = le.fit_transform(df["Education_Level"])
+# df["Loan_Approved"] = le.fit_transform(df["Loan_Approved"])
 
-#Evaluation 
-print("precision score:",precision_score(y_test,y_pred))
-print("recall score:",recall_score(y_test,y_pred))
-print("f1 score:",f1_score(y_test,y_pred))
-print("confusion matrix:",confusion_matrix(y_test,y_pred))
-print("Accuracy score:",accuracy_score(y_test,y_pred))
-
-#GuassianNB model
-naive_model = GaussianNB()
-naive_model.fit(X_train_scaled,y_train)
-naive_model.predict(X_test_scaled)
-y_pred = naive_model.predict(X_test_scaled)
-
-#Evaluation 
-print("precision score:",precision_score(y_test,y_pred))
-print("recall score:",recall_score(y_test,y_pred))
-print("f1 score:",f1_score(y_test,y_pred))
-print("confusion matrix:",confusion_matrix(y_test,y_pred))
-print("Accuracy score:",accuracy_score(y_test,y_pred))
+# #oNEhOTeNCODING
+# cols = ["Employment_Status","Marital_Status","Loan_Purpose","Property_Area","Gender","Employer_Category"]
+# ohe = OneHotEncoder(drop = "first",sparse_output = False,handle_unknown = "ignore")
+# encoded = ohe.fit_transform(df[cols])
+# encoded_df = pd.DataFrame(encoded,columns = ohe.get_feature_names_out(cols),index = df.index )
+# df = pd.concat([df.drop(columns = cols),encoded_df],axis = 1)
 
 
+# #correlation heat map
+# num_cols = df.select_dtypes(include = "number")
+# corr_matrix = num_cols.corr()
+# plt.figure(figsize = (15,8))
+# sns.heatmap(
+#     corr_matrix,
+#     annot = True,
+#     fmt = ".2f",
+#     cmap = "coolwarm"
+# )
+# num_cols.corr()["Loan_Approved"].sort_values(ascending = False)
+# # corr_matrix
 
-#Feature Enngineering 
-df["DTI_Ratio_sq"] = df["DTI_Ratio"] ** 2
-df["Credit_Score_sq"] = df["Credit_Score"] ** 2
+# #split dataset 
+# X = df.drop(["Loan_Approved"],axis = 1)
+# y = df["Loan_Approved"]
 
-#Split Data
-X = df.drop(columns = ["Loan_Approved","DTI_Ratio","Credit_Score"])
-y = df["Loan_Approved"]
+# #train test split
+# X_train,X_test,y_train,y_test = train_test_split(
+#     X,y, test_size = 0.2 , random_state  =42
+# )
+
+# #Standard Scale
+# scaler = StandardScaler()
+# X_train_scaled = scaler.fit_transform(X_train)
+# X_test_scaled = scaler.transform(X_test)
+
+# #Train linearregression model
+# logs_model = LogisticRegression()
+# logs_model.fit(X_train_scaled,y_train)
+# y_pred = logs_model.predict(X_test_scaled)
+
+# # Evaluation
+# print("precision score:",precision_score(y_test,y_pred))
+# print("recall score:",recall_score(y_test,y_pred))
+# print("f1 score:",f1_score(y_test,y_pred))
+# print("confusion matrix:",confusion_matrix(y_test,y_pred))
+# print("Accuracy score:",accuracy_score(y_test,y_pred))
 
 
-#train test split
+# #knn model
+# knn_model = KNeighborsClassifier(n_neighbors=5)
+# knn_model.fit(X_train_scaled,y_train)
+# y_pred = knn_model.predict(X_test_scaled)
 
-X_train,X_test,y_train,y_test = train_test_split(
-    X,y, test_size = 0.2 , random_state  =42
-)
+# #Evaluation 
+# print("precision score:",precision_score(y_test,y_pred))
+# print("recall score:",recall_score(y_test,y_pred))
+# print("f1 score:",f1_score(y_test,y_pred))
+# print("confusion matrix:",confusion_matrix(y_test,y_pred))
+# print("Accuracy score:",accuracy_score(y_test,y_pred))
 
-# feature scalling
-scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
+# #GuassianNB model
+# naive_model = GaussianNB()
+# naive_model.fit(X_train_scaled,y_train)
+# naive_model.predict(X_test_scaled)
+# y_pred = naive_model.predict(X_test_scaled)
 
-
-# Logistic regression
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import precision_score,confusion_matrix,recall_score,f1_score,accuracy_score
-logs_model = LogisticRegression()
-logs_model.fit(X_train_scaled,y_train)
-y_pred = logs_model.predict(X_test_scaled)
-
-# Evaluation
-print("precision score:",precision_score(y_test,y_pred))
-print("recall score:",recall_score(y_test,y_pred))
-print("f1 score:",f1_score(y_test,y_pred))
-print("confusion matrix:",confusion_matrix(y_test,y_pred))
-print("Accuracy score:",accuracy_score(y_test,y_pred))
+# #Evaluation 
+# print("precision score:",precision_score(y_test,y_pred))
+# print("recall score:",recall_score(y_test,y_pred))
+# print("f1 score:",f1_score(y_test,y_pred))
+# print("confusion matrix:",confusion_matrix(y_test,y_pred))
+# print("Accuracy score:",accuracy_score(y_test,y_pred))
 
 
 
-# knn classifier
-from sklearn.neighbors import KNeighborsClassifier
-knn_model = KNeighborsClassifier(n_neighbors=5)
-knn_model.fit(X_train_scaled,y_train)
-y_pred = knn_model.predict(X_test_scaled)
-#Evaluation 
-print("precision score:",precision_score(y_test,y_pred))
-print("recall score:",recall_score(y_test,y_pred))
-print("f1 score:",f1_score(y_test,y_pred))
-print("confusion matrix:",confusion_matrix(y_test,y_pred))
-print("Accuracy score:",accuracy_score(y_test,y_pred))
+# #Feature Enngineering 
+# df["DTI_Ratio_sq"] = df["DTI_Ratio"] ** 2
+# df["Credit_Score_sq"] = df["Credit_Score"] ** 2
+
+# #Split Data
+# X = df.drop(columns = ["Loan_Approved","DTI_Ratio","Credit_Score"])
+# y = df["Loan_Approved"]
+
+
+# #train test split
+
+# X_train,X_test,y_train,y_test = train_test_split(
+#     X,y, test_size = 0.2 , random_state  =42
+# )
+
+# # feature scalling
+# scaler = StandardScaler()
+# X_train_scaled = scaler.fit_transform(X_train)
+# X_test_scaled = scaler.transform(X_test)
+
+
+# # Logistic regression
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.metrics import precision_score,confusion_matrix,recall_score,f1_score,accuracy_score
+# logs_model = LogisticRegression()
+# logs_model.fit(X_train_scaled,y_train)
+# y_pred = logs_model.predict(X_test_scaled)
+
+# # Evaluation
+# print("precision score:",precision_score(y_test,y_pred))
+# print("recall score:",recall_score(y_test,y_pred))
+# print("f1 score:",f1_score(y_test,y_pred))
+# print("confusion matrix:",confusion_matrix(y_test,y_pred))
+# print("Accuracy score:",accuracy_score(y_test,y_pred))
 
 
 
-# naive bayes
-from sklearn.naive_bayes import GaussianNB
-naive_model = GaussianNB()
-naive_model.fit(X_train_scaled,y_train)
-naive_model.predict(X_test_scaled)
-y_pred = naive_model.predict(X_test_scaled)
-
-#Evaluation 
-print("precision score:",precision_score(y_test,y_pred))
-print("recall score:",recall_score(y_test,y_pred))
-print("f1 score:",f1_score(y_test,y_pred))
-print("confusion matrix:",confusion_matrix(y_test,y_pred))
-print("Accuracy score:",accuracy_score(y_test,y_pred))
-
-#feature Engineering
-
-df["DTI_Ratio_sq"] = df["DTI_Ratio"] ** 2
-df["Credit_Score_sq"] = df["Credit_Score"] ** 2
-
-naive_model = GaussianNB()
-naive_model.fit(X_train_scaled,y_train)
-naive_model.predict(X_test_scaled)
-y_pred = naive_model.predict(X_test_scaled)
-
-#Evaluation 
-print("precision score:",precision_score(y_test,y_pred))
-print("recall score:",recall_score(y_test,y_pred))
-print("f1 score:",f1_score(y_test,y_pred))
-print("confusion matrix:",confusion_matrix(y_test,y_pred))
-print("Accuracy score:",accuracy_score(y_test,y_pred))
+# # knn classifier
+# from sklearn.neighbors import KNeighborsClassifier
+# knn_model = KNeighborsClassifier(n_neighbors=5)
+# knn_model.fit(X_train_scaled,y_train)
+# y_pred = knn_model.predict(X_test_scaled)
+# #Evaluation 
+# print("precision score:",precision_score(y_test,y_pred))
+# print("recall score:",recall_score(y_test,y_pred))
+# print("f1 score:",f1_score(y_test,y_pred))
+# print("confusion matrix:",confusion_matrix(y_test,y_pred))
+# print("Accuracy score:",accuracy_score(y_test,y_pred))
 
 
-df.to_csv("loan.csv")
+
+# # naive bayes
+# from sklearn.naive_bayes import GaussianNB
+# naive_model = GaussianNB()
+# naive_model.fit(X_train_scaled,y_train)
+# naive_model.predict(X_test_scaled)
+# y_pred = naive_model.predict(X_test_scaled)
+
+# #Evaluation 
+# print("precision score:",precision_score(y_test,y_pred))
+# print("recall score:",recall_score(y_test,y_pred))
+# print("f1 score:",f1_score(y_test,y_pred))
+# print("confusion matrix:",confusion_matrix(y_test,y_pred))
+# print("Accuracy score:",accuracy_score(y_test,y_pred))
+
+# #feature Engineering
+
+# df["DTI_Ratio_sq"] = df["DTI_Ratio"] ** 2
+# df["Credit_Score_sq"] = df["Credit_Score"] ** 2
+
+# naive_model = GaussianNB()
+# naive_model.fit(X_train_scaled,y_train)
+# naive_model.predict(X_test_scaled)
+# y_pred = naive_model.predict(X_test_scaled)
+
+# #Evaluation 
+# print("precision score:",precision_score(y_test,y_pred))
+# print("recall score:",recall_score(y_test,y_pred))
+# print("f1 score:",f1_score(y_test,y_pred))
+# print("confusion matrix:",confusion_matrix(y_test,y_pred))
+# print("Accuracy score:",accuracy_score(y_test,y_pred))
+
+
+# df.to_csv("loan.csv")
+df = pd.read_csv("loan.csv")
