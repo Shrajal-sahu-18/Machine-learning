3fe4a43aa7b55be42eed5eaa51ed493982c42459
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-07-01 23:25:27 +0530

    import decisiontreeclassifier

--- Supervised_learning/Decision_tree/Decision_tree.py
+++ Supervised_learning/Decision_tree/Decision_tree.py
@@ -31,3 +31,6 @@ y = titanic[target]
 X_train,X_test,y_train,y_test = train_test_split(
     X,y , test_size = 0.2,random_state = 42
 )
+
+# Decision tree classifier - no prunning
+from sklearn.tree import DecisionTreeClassifier
