76c42d41d7a77c7bf0d3c62cc8f03d208b38d8ae
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-07-01 23:24:40 +0530

    train test split

--- Supervised_learning/Decision_tree/Decision_tree.py
+++ Supervised_learning/Decision_tree/Decision_tree.py
@@ -27,3 +27,7 @@ titanic["embarked"] = le.fit_transform(titanic["embarked"])
 
 X = titanic[feature]
 y = titanic[target]
+
+X_train,X_test,y_train,y_test = train_test_split(
+    X,y , test_size = 0.2,random_state = 42
+)
