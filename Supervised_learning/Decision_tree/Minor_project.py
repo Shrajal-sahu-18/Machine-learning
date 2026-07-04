a4dfa88872613ac1ef5bbae7c3a9b81b8965e141
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-07-04 18:11:35 +0530

    divide x into csategory

--- Supervised_learning/Decision_tree/Minor_project.py
+++ Supervised_learning/Decision_tree/Minor_project.py
@@ -8,8 +8,13 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import f1_score, classification_report, confusion_matrix
 
+#Load Dataset
 df = pd.read_csv("shop_smart_ecommerce.csv")
 
+#Split Dataset
 X = df.drop(columns = ["Revenue"])
 y = df["Revenue"]
 
+#Divide category
+num_cols = X.select_dtypes(include =["int64","float64"]).columns
+cate_cols = X.select_dtypes(include =["category","object","bool"]).columns
