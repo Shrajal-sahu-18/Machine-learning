f3b573444b5f5918adbc7c0d7371205fc402d3e9
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-07-04 18:09:47 +0530

    split dataset

--- Supervised_learning/Decision_tree/Minor_project.py
+++ Supervised_learning/Decision_tree/Minor_project.py
@@ -9,3 +9,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.metrics import f1_score, classification_report, confusion_matrix
 
 df = pd.read_csv("shop_smart_ecommerce.csv")
+
+X = df.drop(columns = ["Revenue"])
+y = df["Revenue"]
+
