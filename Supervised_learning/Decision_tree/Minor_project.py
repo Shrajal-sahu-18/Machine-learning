0d1555cbb4b4b492bb4313eaaeebf16253a250d9
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-07-04 18:04:45 +0530

    load dataset

--- Supervised_learning/Decision_tree/Minor_project.py
+++ Supervised_learning/Decision_tree/Minor_project.py
@@ -7,3 +7,5 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import f1_score, classification_report, confusion_matrix
+
+df = pd.read_csv("shop_smart_ecommerce.csv")
