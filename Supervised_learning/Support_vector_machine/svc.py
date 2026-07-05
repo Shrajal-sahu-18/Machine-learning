84a571098658f2b15b531c5a56ba629bae406a23
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-07-05 21:46:31 +0530

    load dataset

--- Supervised_learning/Support_vector_machine/svc.py
+++ Supervised_learning/Support_vector_machine/svc.py
@@ -2,3 +2,6 @@ import pandas as pd
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
+
+#Load Dataset
+df = datasets.load_iris(as_frame = True).frame
