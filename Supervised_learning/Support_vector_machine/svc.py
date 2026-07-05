b9199b3fec9168f1a561028dcc234c56fd9ad2bb
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-07-05 21:49:58 +0530

    Scaled the data

--- Supervised_learning/Support_vector_machine/svc.py
+++ Supervised_learning/Support_vector_machine/svc.py
@@ -19,3 +19,10 @@ y = df["target"]
 X_train,X_test,y_train,y_test = train_test_split(
     X,y, test_size = 0.3,random_state = 42,stratify = y
 ) 
+
+
+#Scaling 
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
