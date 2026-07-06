8d6d6b2b0180b65be6cf2d9966761df1a21a5352
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-07-06 22:27:44 +0530

    load dataset

--- Supervised_learning/Random_forest/random_forest.py
+++ Supervised_learning/Random_forest/random_forest.py
@@ -6,3 +6,6 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
+
+#Load dataset
+titanic = sns.load_dataset("titanic")
