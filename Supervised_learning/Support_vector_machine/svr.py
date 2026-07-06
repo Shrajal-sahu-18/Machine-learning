1e210b8023b391b4bd12a240bb45d83a0e51168c
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-07-06 22:15:55 +0530

    scalling preprocess data

--- Supervised_learning/Support_vector_machine/svr.py
+++ Supervised_learning/Support_vector_machine/svr.py
@@ -16,3 +16,8 @@ y = df["target"]
 #Train test split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=42)
+
+# scalling 
+y_scaler = StandardScaler()
+y_train_scaled = y_scaler.fit_transform(y_train.values.reshape(-1,1)).ravel()
+y_test_scaled = y_scaler.transform(y_test.values.reshape(-1,1)).ravel()
