49ca329d021723bbe1250b2db2e2f4fd4985f982
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-07-06 22:09:19 +0530

    load datasets

--- Supervised_learning/Support_vector_machine/svr.py
+++ Supervised_learning/Support_vector_machine/svr.py
@@ -4,3 +4,7 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 from sklearn.preprocessing import StandardScaler
+
+#load datasets
+df = datasets.load_diabetes(as_frame = True).frame
+df.head()
