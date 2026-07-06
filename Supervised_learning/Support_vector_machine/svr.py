76c06b9ef205bc1d73e05510afbc8f1d63225427
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-07-06 22:11:13 +0530

    train test split

--- Supervised_learning/Support_vector_machine/svr.py
+++ Supervised_learning/Support_vector_machine/svr.py
@@ -12,3 +12,7 @@ df.head()
 #Split dataset
 X = df.drop(["target"],axis = 1)
 y = df["target"]
+
+#Train test split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42)
