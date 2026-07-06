a75d7dc41111580d086c5b709090d9016e7afc20
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-07-06 22:08:10 +0530

    import module

---
+++ Supervised_learning/Support_vector_machine/svr.py
@@ -0,0 +1,6 @@
+from sklearn.svm import SVR
+import pandas as pd
+from sklearn import datasets 
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+from sklearn.preprocessing import StandardScaler
