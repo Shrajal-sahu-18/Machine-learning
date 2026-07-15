74331546d51bfd3b74063894d704d4db0421d0fa
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-07-15 22:49:30 +0530

    Genrate Random data for testing

--- Supervised_learning/Ensemble_boosting/gradient_boosting.py
+++ Supervised_learning/Ensemble_boosting/gradient_boosting.py
@@ -1,3 +1,11 @@
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 from sklearn.datasets import make_regression
+
+#Genrate data
+X,y= make_regression(
+    n_samples = 100,
+    n_features = 10,
+    noise = 20,
+    random_state = 42
+)
