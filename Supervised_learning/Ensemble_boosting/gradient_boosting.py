292998a44b1d753ee9a2ca59377233bcae334a90
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-07-17 20:43:35 +0530

    create model

--- Supervised_learning/Ensemble_boosting/gradient_boosting.py
+++ Supervised_learning/Ensemble_boosting/gradient_boosting.py
@@ -44,3 +44,9 @@ X_train,X_test,y_train,y_test = train_test_split(
 )
 
 from sklearn.ensemble import GradientBoostingClassifier
+gbc = GradientBoostingClassifier(
+    n_estimators=150,
+    learning_rate = 0.1,
+    max_depth = 3,
+    random_state = 42
+)
