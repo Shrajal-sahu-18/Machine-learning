24eb971740c08c99a69e117357d62939d96fd764
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-08-06 15:29:10 +0530

    Genrate random claasification data

--- Supervised_learning/Ensemble_boosting/adaboost.py
+++ Supervised_learning/Ensemble_boosting/adaboost.py
@@ -3,3 +3,12 @@ from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score,classification_report
+
+#genrate Data
+X,y = make_classification(
+    n_samples = 500,
+    n_features = 20,
+    n_informative = 10,
+    n_redundant = 2,
+    random_state = 42
+)
