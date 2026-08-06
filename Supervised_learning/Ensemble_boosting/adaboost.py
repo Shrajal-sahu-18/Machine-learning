e2ea7259cf3a85cdf8e823552030086c576235f4
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-08-06 15:29:46 +0530

    train test split

--- Supervised_learning/Ensemble_boosting/adaboost.py
+++ Supervised_learning/Ensemble_boosting/adaboost.py
@@ -12,3 +12,8 @@ X,y = make_classification(
     n_redundant = 2,
     random_state = 42
 )
+
+#Train test split
+X_train,X_test,y_train,y_test = train_test_split(
+    X,y,test_size = 0.3,random_state = 42
+)
