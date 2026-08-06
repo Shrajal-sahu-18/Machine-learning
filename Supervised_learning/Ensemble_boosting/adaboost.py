f5bc5a408ad8f20e007466e2dd56e0d8b98a0ee2
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-08-06 15:43:35 +0530

    adaboostregressor

--- Supervised_learning/Ensemble_boosting/adaboost.py
+++ Supervised_learning/Ensemble_boosting/adaboost.py
@@ -57,3 +57,10 @@ X_train,X_test,y_train,y_test = train_test_split(
 base_model = DecisionTreeRegressor(
     max_depth = 3
 )
+
+
+abr = AdaBoostRegressor(
+    estimator = base_model,
+    n_estimators = 200,
+    random_state = 42
+)
