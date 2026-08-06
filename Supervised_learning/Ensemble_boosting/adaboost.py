5010cd3de7a1c45a87aac1a8fdd9fc413156135b
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-08-06 15:31:01 +0530

    Base model decision treeclassifier

--- Supervised_learning/Ensemble_boosting/adaboost.py
+++ Supervised_learning/Ensemble_boosting/adaboost.py
@@ -17,3 +17,8 @@ X,y = make_classification(
 X_train,X_test,y_train,y_test = train_test_split(
     X,y,test_size = 0.3,random_state = 42
 )
+
+#DT stump
+base_model = DecisionTreeClassifier(
+    max_depth = 3
+)
