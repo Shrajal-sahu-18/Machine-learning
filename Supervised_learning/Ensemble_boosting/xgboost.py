913c674199e97cdac38cfbc558d45b5bb1c10032
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-08-07 11:37:34 +0530

    genrate random data for classification model

--- Supervised_learning/Ensemble_boosting/xgboost.py
+++ Supervised_learning/Ensemble_boosting/xgboost.py
@@ -1,1 +1,16 @@
 #install xgboost
+# xgboostclassifier
+import xgboost as xgb
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score,classification_report
+
+#Genrate data for classification model
+X,y = make_classification(
+    n_samples = 500,
+    n_features = 20,
+    n_informative = 10,
+    n_redundant = 2,
+    random_state = 42
+    
+)
