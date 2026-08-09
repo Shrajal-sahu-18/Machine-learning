4749ede401b638ef0ac6c531588de17069a2f67c
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-08-09 13:30:31 +0530

    Genrate regression data

--- Supervised_learning/Ensemble_boosting/heterogenous_model/voting_regressor.py
+++ Supervised_learning/Ensemble_boosting/heterogenous_model/voting_regressor.py
@@ -4,3 +4,12 @@ from sklearn.ensemble import VotingRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.svm import SVR
+
+
+
+X,y = make_regression(
+    n_samples = 500,
+    n_features  = 20,
+    n_informative = 5,
+    random_state = 42
+)
