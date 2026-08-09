1b13841484332fceadc2eb2ab33c48aa3a9dd206
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-08-09 13:29:51 +0530

    import module

--- Supervised_learning/Ensemble_boosting/heterogenous_model/voting_regressor.py
+++ Supervised_learning/Ensemble_boosting/heterogenous_model/voting_regressor.py
@@ -1,1 +1,6 @@
 # Voting Regressor
+from sklearn.datasets import make_regression
+from sklearn.ensemble import VotingRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.svm import SVR
