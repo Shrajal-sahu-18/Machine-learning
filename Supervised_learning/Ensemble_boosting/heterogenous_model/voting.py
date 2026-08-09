7dd208d0cc8562f7a2ff05555597a4d44e97a719
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-08-09 13:08:39 +0530

    genrate classification data

--- Supervised_learning/Ensemble_boosting/heterogenous_model/voting.py
+++ Supervised_learning/Ensemble_boosting/heterogenous_model/voting.py
@@ -5,3 +5,11 @@ from sklearn.ensemble import VotingClassifier
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
+
+# Genrate random Classification Data
+X,y = make_classification(
+    n_samples = 500,
+    n_features = 20,
+    n_informative = 5,
+    n_redundant = 2
+)
