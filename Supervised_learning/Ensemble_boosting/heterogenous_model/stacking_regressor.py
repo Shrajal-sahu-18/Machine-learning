4978f556c82c9d904a0dd36ee3aa1c096e0ef19c
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-08-21 19:41:25 +0530

    Train Test Split

--- Supervised_learning/Ensemble_boosting/heterogenous_model/stacking_regressor.py
+++ Supervised_learning/Ensemble_boosting/heterogenous_model/stacking_regressor.py
@@ -4,10 +4,15 @@ from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.svm import SVR
 from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
 
 X,y = make_regression(
     n_samples = 500,
     n_features = 20,
     n_informative = 5,
     random_state = 42
+)
+
+X_train,X_test,y_train,y_test = train_test_split(
+    X,y, test_size = 0.3, random_state = 42
 )
