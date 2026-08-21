2f0417fa300d5d03b9738c3a6b46a93ca31e1844
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-08-21 19:40:10 +0530

    Genrate random regression data

--- Supervised_learning/Ensemble_boosting/heterogenous_model/stacking_regressor.py
+++ Supervised_learning/Ensemble_boosting/heterogenous_model/stacking_regressor.py
@@ -4,3 +4,10 @@ from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.svm import SVR
 from sklearn.metrics import r2_score
+
+X,y = make_regression(
+    n_samples = 500,
+    n_features = 20,
+    n_informative = 5,
+    random_state = 42
+)
