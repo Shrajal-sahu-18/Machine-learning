622952cf340f35b27a7d0aa180ac656502b6008a
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-08-21 19:39:18 +0530

    Import module

---
+++ Supervised_learning/Ensemble_boosting/heterogenous_model/stacking_regressor.py
@@ -0,0 +1,6 @@
+from sklearn.ensemble import StackingRegressor
+from sklearn.datasets import make_regression
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.svm import SVR
+from sklearn.metrics import r2_score
