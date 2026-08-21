395de2ec4df9bc20493fc6b40910e8daf7080639
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-08-21 15:37:26 +0530

    import module

---
+++ Supervised_learning/Ensemble_boosting/heterogenous_model/stacking_clf.py
@@ -0,0 +1,6 @@
+from  sklearn.ensemble import StackingClassifier
+from sklearn.datasets import make_classification
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
