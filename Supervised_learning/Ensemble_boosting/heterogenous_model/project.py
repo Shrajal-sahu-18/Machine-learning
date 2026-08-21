6b6dc06c0b2a0c89192eb59b839d945c5cd6443c
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-08-21 22:55:58 +0530

    Import module

---
+++ Supervised_learning/Ensemble_boosting/heterogenous_model/project.py
@@ -0,0 +1,6 @@
+# Import module
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import recall_score, classification_report,accuracy_score 
