5dd302e76389d0202c745566f062077beff6907a
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-08-21 22:58:42 +0530

    load dataset

--- Supervised_learning/Ensemble_boosting/heterogenous_model/project.py
+++ Supervised_learning/Ensemble_boosting/heterogenous_model/project.py
@@ -4,3 +4,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import recall_score, classification_report,accuracy_score 
+
+#Load Dataset
+df = pd.read_csv("novagen_dataset.csv")
