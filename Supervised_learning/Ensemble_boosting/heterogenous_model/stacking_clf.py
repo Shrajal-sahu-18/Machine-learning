6901f831e09628798767d928b562ce7e0ba966fb
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-08-21 15:38:49 +0530

    Genrate classification data

--- Supervised_learning/Ensemble_boosting/heterogenous_model/stacking_clf.py
+++ Supervised_learning/Ensemble_boosting/heterogenous_model/stacking_clf.py
@@ -4,3 +4,10 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
+
+X,y = make_classification(
+    n_samples = 500,
+    n_features = 20,
+    n_informative = 5,
+    n_redundant = 3
+)
