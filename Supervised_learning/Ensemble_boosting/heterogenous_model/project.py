15e5ee5c9230387db716e2b0a4163bdf29a157fc
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-08-22 23:26:47 +0530

    Recall Score

--- Supervised_learning/Ensemble_boosting/heterogenous_model/project.py
+++ Supervised_learning/Ensemble_boosting/heterogenous_model/project.py
@@ -99,3 +99,4 @@ voting_clf.fit(X_train_scaled,y_train)
 y_pred_vote = voting_clf.predict(X_test_scaled)
 
 print("Voting Classifier Accuracy:", accuracy_score(y_test, y_pred_vote))
+print("Voting Classifier Recall:", recall_score(y_test, y_pred_vote))
