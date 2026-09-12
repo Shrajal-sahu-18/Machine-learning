dd99452f84d73c8cc70594a40865fe47d651b636
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-12 22:45:35 +0530

    genrate random data

--- unsupervised_learning/clustering/dbscan.py
+++ unsupervised_learning/clustering/dbscan.py
@@ -25,3 +25,8 @@ labels = dbscan.fit_predict(X_scaled)
 sns.scatterplot(x = X_scaled[:,0],y = X_scaled[:,2],c = labels)
 
 from sklearn.datasets import make_moons
+X,y = make_moons(
+    n_samples = 300,
+    noise = 0.05,
+    random_state = 42
+)
