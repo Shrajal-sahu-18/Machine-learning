ca723c034d0b4acf649d1c405733d9e1be17972f
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-12 12:47:56 +0530

    import module for dendrogram

--- unsupervised_learning/clustering/hierarchical_clustering.py
+++ unsupervised_learning/clustering/hierarchical_clustering.py
@@ -15,3 +15,7 @@ X_scaled = scaler.fit_transform(X)
 
 # Visulize the data
 sns.scatterplot(x = X_scaled[:,0],y = X_scaled[:,2],c = y)
+
+#Dendrogram
+
+from scipy.cluster.hierarchy import linkage,dendrogram
