211f17f187e51572257db28cb970680d2d614c95
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-13 21:07:03 +0530

    load data

--- unsupervised_learning/dimensionality_reduction/pca.py
+++ unsupervised_learning/dimensionality_reduction/pca.py
@@ -1,4 +1,8 @@
+#import module
 from sklearn.datasets import load_iris
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
+
+# load data
+iris = load_iris()
