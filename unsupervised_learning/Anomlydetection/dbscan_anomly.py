027bc8b8a3c5437db92c746e8798420457621c0d
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-13 21:18:00 +0530

    Genrate random data

--- unsupervised_learning/Anomlydetection/dbscan_anomly.py
+++ unsupervised_learning/Anomlydetection/dbscan_anomly.py
@@ -2,3 +2,9 @@ import seaborn as sns
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import DBSCAN
 from sklearn.datasets import make_moons
+
+X,y = make_moons(
+    n_samples = 500,
+    noise = 0.1,
+    random_state = 42
+)
