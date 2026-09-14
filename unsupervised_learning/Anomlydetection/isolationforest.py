ea5cc041999045da79588f50b3dd0351f37ba9f1
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-14 15:18:21 +0530

    Split dataset

--- unsupervised_learning/Anomlydetection/isolationforest.py
+++ unsupervised_learning/Anomlydetection/isolationforest.py
@@ -3,3 +3,6 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 
 df = pd.read_csv("thyroid_dataset.csv")
+
+X = df.drop("Outlier_label",axis = 1)
+y = df["Outlier_label"]
