125cf703861ef149ab825d5ab23187cd44d7b479
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-14 15:19:38 +0530

    import isolationforest

--- unsupervised_learning/Anomlydetection/isolationforest.py
+++ unsupervised_learning/Anomlydetection/isolationforest.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import IsolationForest
 
 df = pd.read_csv("thyroid_dataset.csv")
 
