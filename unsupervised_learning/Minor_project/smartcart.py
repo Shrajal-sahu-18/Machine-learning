16ffc20b664d0bdd7377f9ab8946e61174878f31
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-16 21:52:16 +0530

    store data in x

--- unsupervised_learning/Minor_project/smartcart.py
+++ unsupervised_learning/Minor_project/smartcart.py
@@ -102,3 +102,5 @@ enc_cols =  ohe.fit_transform(df_clean[cat_cols])
 encoded_df = pd.DataFrame(enc_cols.toarray(),columns = ohe.get_feature_names_out(enc_cols),index = df_clean.index)
 
 df_encoded = pd.concat([df.drop(columns = cat_cols),encoded_df],axis = 1)
+
+X = df_encoded
