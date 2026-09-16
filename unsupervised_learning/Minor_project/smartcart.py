cbdd115ab4c4304a0b90e4ddf08d251697a86b73
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-16 22:06:29 +0530

    import silhouette score

--- unsupervised_learning/Minor_project/smartcart.py
+++ unsupervised_learning/Minor_project/smartcart.py
@@ -6,6 +6,7 @@ from sklearn.preprocessing import OneHotEncoder , StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 from kneed import KneeLocator
+from sklearn.metrics import silhouette_score 
 
 # Read dataset
 df = pd.read_csv("smartcart_customers.csv")
