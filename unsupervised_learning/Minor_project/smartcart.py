12f7c7a499d43c2c94581599d6a3346d096131e4
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-16 22:16:22 +0530

    3d plot

--- unsupervised_learning/Minor_project/smartcart.py
+++ unsupervised_learning/Minor_project/smartcart.py
@@ -175,3 +175,7 @@ agg_clf = AgglomerativeClustering(
 )
 
 labels_agg = agg_clf.fit_predict(X_pca)
+
+fig = plt.figure(figsize = (8,6))
+ax = fig.add_subplot(111,projection = "3d")
+ax.scatter(X_pca[:,0],X_pca[:,1],X_pca[:,2], c = labels_agg)
