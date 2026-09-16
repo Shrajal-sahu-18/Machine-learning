de308324e8a46b25af623d78045f884a321a7988
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-16 21:51:27 +0530

    import standardscaler

--- unsupervised_learning/Minor_project/smartcart.py
+++ unsupervised_learning/Minor_project/smartcart.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder , StandardScaler
 
 # Read dataset
 df = pd.read_csv("smartcart_customers.csv")
